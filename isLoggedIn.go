a964926a256a26dbadb5ccc12352d6c9df3ef51b
Marco Felici <marcofelici91@gmail.com>
2016-06-14 00:17:17 +0200

    Allow only one authenticated session per user

--- isLoggedIn.go
+++ isLoggedIn.go
@@ -26,5 +26,23 @@ func isLoggedIn(w http.ResponseWriter, r *http.Request) (bool, string, error) {
 		return false, "", ErrDB
 	}
 
+	// Expired session
+	auth, err := redis.String(conn.Do("HGET", "webapp:users:"+uid, "auth"))
+	if err != nil {
+		return false, "", ErrDB
+	} else if cookie.Value != auth {
+		// Delete session
+		_, err := conn.Do("DEL", "webapp:users:auth:session:"+cookie.Value)
+		if err != nil {
+			return false, "", ErrDB
+		}
+
+		// Delete cookie
+		newCookie := http.Cookie{Name: "auth", Value: "", MaxAge: -1}
+		http.SetCookie(w, &newCookie)
+
+		return false, "", nil
+	}
+
 	return true, uid, nil
 }
