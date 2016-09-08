74035fdd6ec9859f10119bf1bc6b7328ac73b595
Marco Felici <marcofelici91@gmail.com>
2016-09-08 13:28:11 +0200

    Refactor

--- webapp/accountDeleteHandler.go
+++ webapp/accountDeleteHandler.go
@@ -3,18 +3,9 @@ package main
 import (
 	"log"
 	"net/http"
-	"strings"
-
-	"github.com/garyburd/redigo/redis"
 )
 
 func accountDelete(w http.ResponseWriter, r *http.Request) error {
-	var user = struct {
-		Email    string `redis:"email"`
-		Username string `redis:"username"`
-		Auth     string `redis:"auth"`
-	}{}
-
 	logged, uid, err := isLoggedIn(w, r)
 	if err != nil {
 		return ErrGeneric
@@ -24,49 +15,8 @@ func accountDelete(w http.ResponseWriter, r *http.Request) error {
 		return nil
 	}
 
-	conn := pool.Get()
-	defer conn.Close()
-
-	data, err := redis.Values(conn.Do("HGETALL", "webapp:users:"+uid))
-	if err != nil {
-		return ErrDB
-	}
-
-	err = redis.ScanStruct(data, &user)
-	if err != nil {
-		return ErrDB
-	}
-
-	lname, err := redis.String(conn.Do("HGET", "webapp:users:data:"+uid, "cognome"))
-	if err != nil && err != redis.ErrNil {
-		return ErrDB
-	}
-
-	// Get the messages sent by the user
-	messages, err := redis.Strings(conn.Do("LRANGE", "webapp:users:messages:"+uid, 0, -1))
-	if err != nil && err != redis.ErrNil {
-		return ErrDB
-	}
-
-	// Delete all the data associated with the user
-	conn.Send("MULTI")
-	conn.Send("HDEL", "webapp:users", user.Username)
-	conn.Send("HDEL", "webapp:users:email", user.Email)
-	conn.Send("DEL", "webapp:users:auth:session:"+user.Auth)
-	conn.Send("DEL", "webapp:users:"+uid)
-	conn.Send("DEL", "webapp:users:data:"+uid)
-	conn.Send("DEL", "webapp:users:data:email:"+uid)
-	conn.Send("DEL", "webapp:users:data:url:"+uid)
-	for _, m := range messages {
-		conn.Send("DEL", "webapp:messages:"+m)
-	}
-	conn.Send("DEL", "webapp:users:messages:"+uid)
-	if len(lname) != 0 {
-		conn.Send("SREM", "webapp:users:info:"+strings.ToLower(lname), uid)
-	}
-	_, err = conn.Do("EXEC")
-	if err != nil {
-		return ErrDB
+	if err := deleteAccount(uid); err != nil {
+		return err
 	}
 
 	msg := "L'account è stato eliminato."
---
+++ webapp/deleteAccount.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"strings"
+
+	"github.com/garyburd/redigo/redis"
+)
+
+func deleteAccount(uid string) error {
+	var user = struct {
+		Email    string `redis:"email"`
+		Username string `redis:"username"`
+		Auth     string `redis:"auth"`
+	}{}
+
+	conn := pool.Get()
+	defer conn.Close()
+
+	data, err := redis.Values(conn.Do("HGETALL", "webapp:users:"+uid))
+	if err != nil {
+		return ErrDB
+	}
+
+	err = redis.ScanStruct(data, &user)
+	if err != nil {
+		return ErrDB
+	}
+
+	lname, err := redis.String(conn.Do("HGET", "webapp:users:data:"+uid, "cognome"))
+	if err != nil && err != redis.ErrNil {
+		return ErrDB
+	}
+
+	// Get the messages sent by the user
+	messages, err := redis.Strings(conn.Do("LRANGE", "webapp:users:messages:"+uid, 0, -1))
+	if err != nil && err != redis.ErrNil {
+		return ErrDB
+	}
+
+	// Delete all the data associated with the user
+	conn.Send("MULTI")
+	conn.Send("HDEL", "webapp:users", user.Username)
+	conn.Send("HDEL", "webapp:users:email", user.Email)
+	conn.Send("DEL", "webapp:users:auth:session:"+user.Auth)
+	conn.Send("DEL", "webapp:users:"+uid)
+	conn.Send("DEL", "webapp:users:data:"+uid)
+	conn.Send("DEL", "webapp:users:data:email:"+uid)
+	conn.Send("DEL", "webapp:users:data:url:"+uid)
+	for _, m := range messages {
+		conn.Send("DEL", "webapp:messages:"+m)
+	}
+	conn.Send("DEL", "webapp:users:messages:"+uid)
+	if len(lname) != 0 {
+		conn.Send("SREM", "webapp:users:info:"+strings.ToLower(lname), uid)
+	}
+	_, err = conn.Do("EXEC")
+	if err != nil {
+		return ErrDB
+	}
+
+	return nil
+}
